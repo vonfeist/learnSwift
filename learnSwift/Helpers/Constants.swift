9909fb28271367f284751ee1d9c9a8d6369d48ef
vonfeist <pia_fornfeist@hotmail.de>
2019-05-24 10:35:54 +0200

    Color Enum errors removed

--- learnSwift/Helpers/Constants.swift
+++ learnSwift/Helpers/Constants.swift
@@ -7,13 +7,14 @@
 //
 
 import Foundation
+import UIKit
 
 enum Color {
-    static let primaryColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
-    static let secondaryColor = UIColor.lightGray
+    static let primaryColor: UIColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let secondaryColor: UIColor = UIColor.lightGray
     
     // A visual way to define colours within code files is to use #colorLiteral
     // This syntax will present you with colour picker component right on the code line
     // tertiaryColor = #colorLiteral(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
-    static let tertiaryColor = #colorLiteral(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let tertiaryColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
 }
