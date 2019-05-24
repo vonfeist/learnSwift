a1177c79b7ec1e653eaa710b6d554368f143e498
vonfeist <pia_fornfeist@hotmail.de>
2019-05-24 10:29:55 +0200

    Constant swift, folder structure

---
+++ learnSwift/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  learnSwift
+//
+//  Created by Pia Fornfeist on 24.05.19.
+//  Copyright © 2019 ethinking. All rights reserved.
+//
+
+import Foundation
+
+enum Color {
+    static let primaryColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let secondaryColor = UIColor.lightGray
+    
+    // A visual way to define colours within code files is to use #colorLiteral
+    // This syntax will present you with colour picker component right on the code line
+    // tertiaryColor = #colorLiteral(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let tertiaryColor = #colorLiteral(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+}
